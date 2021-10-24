457d4bcea94b9cf9325d80098184a9ba3a8f8d0e
ariaocho888 <ariaocho@gmail.com>
2021-10-23 20:59:55 -0500

    modelos , admin evento, registro y catalogos

--- apps/amcm/admin.py
+++ apps/amcm/admin.py
@@ -100,12 +100,50 @@ class CuadraAdmin(admin.ModelAdmin):
     model = Cuadras
     #fields = ('nombre', 'representante', 'telefono', 'celular', 'correoElectronico', 'observaciones')
     actions = None
-    extra = 1
     list_per_page = 20
     inlines = [EjemplarInlineAdmin, ]
     list_display = ('nombre', 'representante', 'telefono', 'celular', 'correoElectronico',)
     fieldsets = (
         (('Cuadras'), {'fields': ('nombre', 'representante', 'telefono', 'celular', 'correoElectronico', 'observaciones',)}),)
+
+
+class CondicionesEventoInlineAdmin(admin.TabularInline):
+    model = CondicionesEvento
+    fields = ('limite', 'tipoCondicion', 'valor', 'especificacion', )
+    actions = None
+    extra = 1
+    list_per_page = 20
+
+    # fieldsets = (
+    #     (('Ejemplares'),
+    #      {'fields': ('nombre', 'edad', 'peso', 'sexo', 'nacionalidad', 'color', 'padre', 'madre',)}),)
+
+
+
+class FechasEventoInlineAdmin(admin.TabularInline):
+    model = FechasEvento
+    fields = ('tipoFecha', 'fecha', 'evento' )
+    actions = None
+    extra = 1
+    list_per_page = 20
+
+    # fieldsets = (
+    #     (('Ejemplares'),
+    #      {'fields': ('nombre', 'edad', 'peso', 'sexo', 'nacionalidad', 'color', 'padre', 'madre',)}),)
+
+
+
+class EventoAdmin(admin.ModelAdmin):
+    model = Evento
+    # fields = ('nombre', 'representante', 'telefono', 'celular', 'correoElectronico', 'observaciones')
+    actions = None
+    list_per_page = 20
+    inlines = [FechasEventoInlineAdmin, CondicionesEventoInlineAdmin, ]
+    list_display = ('nombre', 'yardas', 'bolsa', 'fondo', 'tipoEvento',)
+    fieldsets = (
+        (('Evento'),
+         {'fields': ('nombre' , 'yardas' , 'descripcion', 'bolsa', 'fondo', 'temporada', 'tipoEvento', 'observaciones','descuento' )}),)
+
 
 
 admin.site.register(Cuotas, CuotaAdmin)
@@ -115,3 +153,10 @@ admin.site.register(Sexo, SexoAdmin)
 admin.site.register(Nacionalidad, NacionalidadAdmin)
 admin.site.register(Cuadras, CuadraAdmin)
 admin.site.register(Ejemplares, EjemplarAdmin)
+admin.site.register(Evento, EventoAdmin)
+admin.site.register(TipoFecha)
+admin.site.register(CuotaEvento)
+admin.site.register(RegistroCuotaEvento)
+admin.site.register(Limite)
+admin.site.register(TipoCondicion)
+admin.site.register(TipoEvento)
