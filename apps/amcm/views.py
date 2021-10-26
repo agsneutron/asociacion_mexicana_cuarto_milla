0073b82a5764962f023119ca23f65e336567537f
agsneutron <ags_neutron@hotmail.com>
2021-10-26 10:22:06 -0500

    listview eventos

--- apps/amcm/views.py
+++ apps/amcm/views.py
@@ -1,14 +1,16 @@
-# -*- encoding: utf-8 -*-
+
+# coding=utf-8
 """
 Copyright (c) 2021 - AriSan - FusionTI
 """
+
 from django.views.generic import ListView
-from amcm.models import Evento
+from apps.amcm.models import Evento
 import operator
 from django.db.models import Q
 from __future__ import unicode_literals
-from django.db.models import Sum
-
+from django.core.exceptions import PermissionDenied
+from functools import reduce
 
 class EventoListView(ListView):
     model = Evento
@@ -38,29 +40,29 @@ class EventoListView(ListView):
             query_list = query.split()
             result = result.filter(
                 reduce(operator.and_,
-                       (Q(nombreProyecto__icontains=q) for q in query_list)) |
+                       (Q(nombre__icontains=q) for q in query_list)) |
                 reduce(operator.and_,
-                       (Q(ubicacion_calle__icontains=q) for q in query_list)))
+                       (Q(temporada__icontains=q) for q in query_list)))
 
         else:
-            ProjectListView.query = ''
+            EventoListView.query = ''
 
-        if len(projects_array) > 0:
-            result = result.filter(reduce(operator.or_, (Q(pk=q) for q in projects_array)))
+        if len(eventos_array) > 0:
+            result = result.filter(reduce(operator.or_, (Q(pk=q) for q in eventos_array)))
         else:
-            result = Project.objects.none()
+            result = Evento.objects.none()
 
         return result
 
     def get_context_data(self, **kwargs):
-        context = super(ProjectListView, self).get_context_data(**kwargs)
-        context['title_list'] = ProjectListView.title_list
-        context['query'] = ProjectListView.query
-        context['query_string'] = '&q=' + ProjectListView.query
-        context['has_query'] = (ProjectListView.query is not None) and (ProjectListView.query != "")
+        context = super(EventoListView, self).get_context_data(**kwargs)
+        context['title_list'] = EventoListView.title_list
+        context['query'] = EventoListView.query
+        context['query_string'] = '&q=' + EventoListView.query
+        context['has_query'] = (EventoListView.query is not None) and (EventoListView.query != "")
         return context
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.has_perm('erp.view_list_project'):
             raise PermissionDenied
-        return super(ProjectListView, self).dispatch(request, args, kwargs)
+        return super(EventoListView, self).dispatch(request, args, kwargs)
