2898a43e6a9add034be65c1a488f2d1be217d330
agsneutron <ags_neutron@hotmail.com>
2022-03-16 10:14:13 -0600

    recibo con cambio de nombre

--- apps/amcm/api.py
+++ apps/amcm/api.py
@@ -61,7 +61,7 @@ def html_to_pdf(content, output):
 class Render():
     @staticmethod
     def render(path, params):
-        filename = 'recibo'+ str(datetime.datetime.now()) +'.pdf'
+        filename = 'recibo_'+ str(params['no_recibo']) +'.pdf'
         template = get_template(path)
         html = template.render(params)
         stream = io.buffer = BytesIO()
